b5e4b5570876474a5a707c91aa7df352b7a9be2b
GMZlate <zgu14@outlook.com>
2018-04-25 10:50:47 -0500

    Update foil_tests.erl

--- test/foil_tests.erl
+++ test/foil_tests.erl
@@ -28,8 +28,7 @@ foil_test() ->
 
     ok = foil:load(test),
     {error, module_not_found} = foil:load(test2),
-    Value_bin = term_to_binary(value),
-    {ok, Value_bin} = test_foil:lookup(key),
+    {ok, value} = test_foil:lookup(key),
     {ok, [<<"foo">>, <<"bar">>]} = foil:lookup(test, key2),
     {ok, {1, 1.234}} = foil:lookup(test, key3),
     {error, module_not_found} = foil:lookup(test2, key),
